320ff5aabddec7172620c541094d200d773518b2
ROSHAN5713 <97423665+ROSHAN5713@users.noreply.github.com>
2025-05-10 09:52:25 +0530

    Add files via upload

--- compatiblearray.java
+++ compatiblearray.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 
-public class compatiblearray {
-    public static void main(String[] args) {
+public class compatiblearray{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter range of 1st array: ");
+        System.out.print("enter range: ");
         int n = sc.nextInt();
         int arr1[] = new int[n];
-        System.out.println("Enter " + n + " elements for 1st array:");
-        int i,j;
-        for (i = 0; i < n; i++) {
+        int arr2[]= new int[n]; 
+        int count=0;
+        System.out.print("enter elements of 1st array: ");
+        for(int i = 0; i < n ; i++){
             arr1[i] = sc.nextInt();
         }
-        System.out.print("Enter range of 2nd array: ");
-        int m = sc.nextInt();
-        int arr2[]= new int[m];
-        System.out.println("Enter " + m + " elements for 2nd array:");
-        for (j = 0; j< m; j++){
-            arr2[j] = sc.nextInt();
+        System.out.print("enter elements of 2nd array: ");
+        for(int i = 0; i < n ; i++){
+            arr2[i] = sc.nextInt();
         }
-        if(i == j && arr1[i] >= arr2[j]){
-            System.out.println("Array 1 is compatible with Array 2");
+        for(int i = 0; i < n ; i++){
+            if(arr1[i]>=arr2[i]){
+                count++;
+            }
+        }
+        if(count == n){
+            System.out.println("Compatible");
         }
         else{
-            System.out.println("Array 1 is not compatible with Array 2");
+            System.out.println("Not Compatible");
         }
     }
 }
